03642c9b9f759d9e71c62fb05664536d9aeaf508
00abdoreda <bodybody950@gmail.com>
2023-07-03 19:38:09 +0300

    ready

---
+++ lib/intro.dart
@@ -0,0 +1,83 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:intro_slider/intro_slider.dart';
+import 'package:get_storage/get_storage.dart';
+
+import 'main.dart';
+
+class IntroScreenDefault extends StatefulWidget {
+  const IntroScreenDefault({Key? key}) : super(key: key);
+
+  @override
+  IntroScreenDefaultState createState() => IntroScreenDefaultState();
+}
+
+class IntroScreenDefaultState extends State<IntroScreenDefault> {
+  final introdate=GetStorage();
+
+  void endintro(context){
+
+   Navigator.pop (context);
+
+
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_)=>MyHomePage(title: 'main',))
+    );
+
+  }
+  List<ContentConfig> listContentConfig = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    listContentConfig.add(
+      const ContentConfig(
+        title: "Skin diseases",
+        description:
+        "Skin diseases are highly dangerous and some of them are infectious",
+        pathImage: "images/l1.png",
+        backgroundColor: Color(0xff23cff5),
+      ),
+    );
+    listContentConfig.add(
+      const ContentConfig(
+        title: "AI",
+        description:
+        "Our mobile application helps people identify and differ between the diseases using AI",
+        pathImage: "images/l3.png",
+        backgroundColor: Color(0xffb047a8),
+      ),
+    );
+    listContentConfig.add(
+      const ContentConfig(
+        title: "Application",
+        description:
+        "application can be a faster and more reliable way to diagnose the disease",
+        pathImage: "images/l2.png",
+        backgroundColor: Color(0xff9932CC),
+      ),
+    );
+  }
+
+  void onDonePress() {
+
+    endintro(context);
+  }
+
+  void onSkip(){
+
+    endintro(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IntroSlider(
+      key: UniqueKey(),
+      listContentConfig: listContentConfig,
+      onDonePress: onDonePress,
+      onSkipPress: onSkip,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,39 @@
 import 'package:flutter/material.dart';
-
-void main() {
+import 'package:get_storage/get_storage.dart';
+import 'intro.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+void main() async {
+  await GetStorage.init();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final introdate2=GetStorage();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    introdate2.writeIfNull('displayed', false);
+
+
+  }
+
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return  GetMaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      home: introdate2.read('displayed')?MyHomePage(title: 'hello',) : IntroScreenDefault() ,
+
+
     );
   }
 }
@@ -47,7 +58,14 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  final introdate = GetStorage();
+  @override
+  void initState() {
+    super.initState();
 
+    introdate.write('displayed',true);
+    print( introdate.read('displayed'));
+  }
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
