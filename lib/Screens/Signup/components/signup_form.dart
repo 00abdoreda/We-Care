8a9868a1e9148dbdb9d54eac85bc7c0e329eb965
emadmoenes <emadmoanes200@gmail.com>
2023-07-05 00:28:59 +0200

    initial

--- lib/Screens/Signup/components/signup_form.dart
+++ lib/Screens/Signup/components/signup_form.dart
@@ -4,16 +4,51 @@ import '../../../components/already_have_an_account_acheck.dart';
 import '../../../constants.dart';
 import '../../Login/login_screen.dart';
 
-class SignUpForm extends StatelessWidget {
+class SignUpForm extends StatefulWidget {
   const SignUpForm({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<SignUpForm> createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  @override
   Widget build(BuildContext context) {
     return Form(
       child: Column(
         children: [
+
+          TextFormField(
+            keyboardType: TextInputType.name,
+            textInputAction: TextInputAction.next,
+            cursorColor: kPrimaryColor,
+            onSaved: (firstname) {},
+            decoration: InputDecoration(
+              hintText: "First name",
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(defaultPadding),
+                // child: Icon(Icons.person),
+              ),
+            ),
+          ),
+          const SizedBox(height: defaultPadding ),
+          TextFormField(
+            keyboardType: TextInputType.name,
+            textInputAction: TextInputAction.next,
+            cursorColor: kPrimaryColor,
+            onSaved: (lastname) {},
+            decoration: InputDecoration(
+              hintText: "Last name",
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(defaultPadding),
+                // child: Icon(Icons.abc),
+              ),
+            ),
+          ),
+
+          const SizedBox(height: defaultPadding ),
           TextFormField(
             keyboardType: TextInputType.emailAddress,
             textInputAction: TextInputAction.next,
@@ -23,25 +58,73 @@ class SignUpForm extends StatelessWidget {
               hintText: "Your email",
               prefixIcon: Padding(
                 padding: const EdgeInsets.all(defaultPadding),
-                child: Icon(Icons.person),
+                child: Icon(Icons.email),
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: defaultPadding),
-            child: TextFormField(
-              textInputAction: TextInputAction.done,
-              obscureText: true,
-              cursorColor: kPrimaryColor,
-              decoration: InputDecoration(
-                hintText: "Your password",
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.all(defaultPadding),
-                  child: Icon(Icons.lock),
-                ),
+
+          const SizedBox(height: defaultPadding ),
+          TextFormField(
+            keyboardType: TextInputType.datetime,
+            textInputAction: TextInputAction.next,
+            cursorColor: kPrimaryColor,
+            onSaved: (age) {},
+            decoration: InputDecoration(
+              hintText: "Your age",
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.date_range),
               ),
             ),
           ),
+
+          const SizedBox(height: defaultPadding ),
+          TextFormField(
+            keyboardType: TextInputType.number,
+            textInputAction: TextInputAction.next,
+            cursorColor: kPrimaryColor,
+            onSaved: (phone) {},
+            decoration: InputDecoration(
+              hintText: "Phone number",
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.phone),
+              ),
+            ),
+          ),
+
+          const SizedBox(height: defaultPadding ),
+          TextFormField(
+            keyboardType: TextInputType.number,
+            textInputAction: TextInputAction.next,
+            cursorColor: kPrimaryColor,
+            onSaved: (id) {},
+            decoration: InputDecoration(
+              hintText: "National ID",
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.person_2),
+              ),
+            ),
+          ),
+
+          const SizedBox(height: defaultPadding ),
+          TextFormField(
+            keyboardType: TextInputType.text,
+            textInputAction: TextInputAction.next,
+            cursorColor: kPrimaryColor,
+            onSaved: (password) {},
+            decoration: InputDecoration(
+              hintText: "Password",
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.password),
+              ),
+            ),
+          ),
+
+
+
           const SizedBox(height: defaultPadding / 2),
           ElevatedButton(
             onPressed: () {},
@@ -66,3 +149,137 @@ class SignUpForm extends StatelessWidget {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+//
+// class SignupPage extends StatefulWidget {
+//   @override
+//   _SignupPageState createState() => _SignupPageState();
+// }
+//
+// class _SignupPageState extends State<SignupPage> {
+//   final _formKey = GlobalKey<FormState>();
+//   late String _firstName;
+//   late String _lastName;
+//   late String _email;
+//   late String _password;
+//   late String _id;
+//   late String _age;
+//   late String _mobile;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Sign up'),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Form(
+//           key: _formKey,
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.stretch,
+//             children: <Widget>[
+//               TextFormField(
+//                 decoration: InputDecoration(labelText: 'First Name'),
+//                 validator: (value) {
+//                   if (value!.isEmpty) {
+//                     return 'Please enter your first name';
+//                   }
+//                   return null;
+//                 },
+//                 onSaved: (value) {
+//                   _firstName = value!;
+//                 },
+//               ),
+//               TextFormField(
+//                 decoration: InputDecoration(labelText: 'Last Name'),
+//                 validator: (value) {
+//                   if (value!.isEmpty) {
+//                     return 'Please enter your last name';
+//                   }
+//                   return null;
+//                 },
+//                 onSaved: (value) {
+//                   _lastName = value!;
+//                 },
+//               ),
+//               TextFormField(
+//                 decoration: InputDecoration(labelText: 'Email'),
+//                 validator: (value) {
+//                   if (value!.isEmpty) {
+//                     return 'Please enter your email';
+//                   }
+//                   return null;
+//                 },
+//                 onSaved: (value) {
+//                   _email = value!;
+//                 },
+//               ),
+//               TextFormField(
+//                 decoration: InputDecoration(labelText: 'Password'),
+//                 obscureText: true,
+//                 validator: (value) {
+//                   if (value!.isEmpty) {
+//                     return 'Please enter a password';
+//                   }
+//                   return null;
+//                 },
+//                 onSaved: (value) {
+//                   _password = value!;
+//                 },
+//               ),
+//               TextFormField(
+//                 decoration: InputDecoration(labelText: 'ID'),
+//                 validator: (value) {
+//                   if (value!.isEmpty) {
+//                     return 'Please enter your ID';
+//                   }
+//                   return null;
+//                 },
+//                 onSaved: (value) {
+//                   _id = value!;
+//                 },
+//               ),
+//               TextFormField(
+//                 decoration: InputDecoration(labelText: 'Age'),
+//                 keyboardType: TextInputType.number,
+//                 validator: (value) {
+//                   if (value!.isEmpty) {
+//                     return 'Please enter your age';
+//                   }
+//                   return null;
+//                 },
+//                 onSaved: (value) {
+//                   _age = value!;
+//                 },
+//               ),
+//               TextFormField(
+//                 decoration: InputDecoration(labelText: 'Mobile'),
+//                 keyboardType: TextInputType.phone,
+//                 validator: (value) {
+//                   if (value!.isEmpty) {
+//                     return 'Please enter your mobile number';
+//                   }
+//                   return null;
+//                 },
+//                 onSaved: (value) {
+//                   _mobile = value!;
+//                 },
+//               ),
+//               ElevatedButton(
+//                 onPressed: () {
+//                   if (_formKey.currentState!.validate()) {
+//                     _formKey.currentState!.save();
+//                     // Do something with the form data
+//                   }
+//                 },
+//                 child: Text('Sign up'),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
